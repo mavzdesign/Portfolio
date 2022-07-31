d51a01f70bb6f82c47fa8331c07b1fe4f97030b5
unknown <adeolarhowlabi@gmail.com>
2022-07-31 12:32:48 +0100

    change

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -1,13 +1,17 @@
-import React, { useRef }  from 'react'
+import React from 'react'
 import "./contact.css"
 import { MdOutlineMail } from 'react-icons/md';
 import { SiWhatsapp } from 'react-icons/si';
 import emailjs from '@emailjs/browser';
+import { useForm } from "react-hook-form";
+import swal from 'sweetalert';
+// import { yupResolver } from "@hookform/resolvers/yup";
 
 
 
 
 const Contact = () => {
+  const { register,resetField, handleSubmit } = useForm({ shouldUseNativeValidation: true });
   const contactbox =
   [
     {
@@ -28,17 +32,32 @@ const Contact = () => {
 
   ]
 
-  const form = useRef();
+  // const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const sendEmail = (data) => {
+    // e.preventDefault();
+    console.log(data)
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.send('service_v03va6u', 'template_omflxof', data, 'KiXGdGFVQlFJ50RMr')
       .then((result) => {
-          console.log(result.text);
+        if(result.status === 200){
+          swal({
+            title: "Sent",
+            text: "Your message to Olaoluwa was sent successfully ",
+            icon: "success",
+          });
+          resetField("name")
+          resetField("fullName")
+          resetField("msg")
+
+        }
+          // console.log(result);
+
       }, (error) => {
           console.log(error.text);
+          
       });
+      
   };
   return (
     <div id='contact' className='container'>
@@ -68,10 +87,14 @@ const Contact = () => {
             }
           </article>
           <div className='contact-form'>
-              <form  onSubmit={sendEmail} className='contact-main-form' ref={form}>
-                <input type="text" placeholder='Your Fullname' required/>
-                <input type="text" placeholder='Your Name' required/>
-                <textarea name="message" id="" cols="30" rows="10" required></textarea>
+              <form  onSubmit={handleSubmit(sendEmail)} className='contact-main-form' >
+                <input type="text" placeholder='Your Fullname'  {...register("fullName", { required: "Please enter your  fullName." })}/>
+                <input type="text" placeholder='Your Name' 
+                  {...register("name", { required: "Please enter your  name." })}/>
+                <textarea name="message" id="" cols="30" rows="10"
+                  {...register("msg", { required: "Please enter your message." })}
+                
+                required></textarea>
                 <button type='submit' className='btn btn-main'>Send Message</button>
                 {/* <a href="" className='btn btn-main'>Send Message</a> */}
               </form>
